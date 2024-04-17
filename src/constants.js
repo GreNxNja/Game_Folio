d3529c03d20a7e40fc7f9b5daf65e675b83e29ff
Basilisk <prith.tiu13@gmail.com>
2024-04-17 11:39:24 +0530

    Done And Dusted

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Next.js, Flutter, and Dart these days. I've made a couple of projects in TypeScript. Anyway, regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/GreNxNja" target="_blank">Github</a>!`,
+  "cs-degree": `Check out my prized possession—the CS degree! It's not just a fancy piece of paper; it's a ticket to the digital wonderland! I proudly hang it on my wall, a testament to my conquests in the realm of algorithms and binary mysteries. Though it's steeped in theory, it's the foundation of my tech adventures!`,
+  "sofa-table": `That's my sofa. It's my sanctuary, where I kick back, relax, and let the stresses of the day melt away. You might find me here binge-watching my favorite shows or lost in the pages of a good book.`,
+  tv: `That's my TV. Lately, I've been hooked on tech YouTubers like:
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>,
+   <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>, and
+   <a href="https://www.youtube.com/@dudeperfect" target="_blank">Dude Perfect</a>!`,
+  bed: `Welcome to my sleep haven! This is where dreams are made—both literally and figuratively. Some of my best ideas strike when I'm cozied up here, and let me tell you, they're worth losing sleep over!`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/GreNxNja/PrithFolio/blob/main/README.md" target="_blank">Check it out?</a>
+  Contact me at dev.prith13@gmail.com if you have any interesting job opportunities!`,
+  projects: `Gain insights into my professional journey through the projects I've worked on. Explore the challenges faced, solutions implemented, and the impact created. Each project serves as a milestone in my career, showcasing my skills and expertise in action:
+   <a href="https://github.com/GreNxNja/Mirofy" target="_blank">Mirofy</a>,
+   <a href="https://github.com/GreNxNja/Spotify_Clone" target="_blank">Spotify Clone</a>, and
+   <a href="https://github.com/GreNxNja/Myrtle" target="_blank">Myrtle</a>!`,
+  library: `Welcome to my library! Among the shelves filled with gaming CDs, you'll find a treasure trove of knowledge waiting to be explored. With around 300 disks in my collection, each one tells a story of adventure, strategy, and countless hours of gaming glory.`,
+  exit: `If you want to exit Prithwijit's portfolio, just leave XD.`,
+};
