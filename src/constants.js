caebea02c73118ec17a8106989958fd454e261cd
Basilisk <prith.tiu13@gmail.com>
2024-04-25 12:06:01 +0530

    update

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const scaleFactor = 4;
 export const dialogueData = {
   pc: `This is my PC. I work mostly in Next.js, Flutter, and Dart these days. I've made a couple of projects in TypeScript. Anyway, regardless of the language, I just like programming.
     Here is my <a href="https://github.com/GreNxNja" target="_blank">Github</a>!`,
-  "cs-degree": `Check out my prized possession—the CS degree! It's not just a fancy piece of paper; it's a ticket to the digital wonderland! I proudly hang it on my wall, a testament to my conquests in the realm of algorithms and binary mysteries. Though it's steeped in theory, it's the foundation of my tech adventures!`,
+  "cs-degree": `My to-do list keeps track of my ongoing and upcoming projects in computer science, guiding me through the dynamic landscape of technology.`,
   "sofa-table": `That's my sofa. It's my sanctuary, where I kick back, relax, and let the stresses of the day melt away. You might find me here binge-watching my favorite shows or lost in the pages of a good book.`,
   tv: `That's my TV. Lately, I've been hooked on tech YouTubers like:
    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>,
